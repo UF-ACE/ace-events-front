55d9619480699729ec2a6426ff64b6b071084bd7
Christopher Miller <admin@mctherealm.net>
2020-01-22 14:22:10 -0500

    Fixed use of old router to redirect

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,17 +3,9 @@ const common = require('./webpack.common.js');
 const TerserJSPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const Visualizer = require('webpack-visualizer-plugin');
-const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
-  plugins: [
-    new WorkboxPlugin.GenerateSW({
-      clientsClaim: true,
-      skipWaiting: true,
-      importWorkboxFrom: 'local'
-    }),
-  ],
   optimization: {
     minimize: true,
     minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})]
